653436582046daac6358f1bc43c9ce92fc9d41e0
dannyho-git <tingho@yahoo.com>
2021-02-18 02:39:03 +0000

    First change made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
     
+    if (Number(principal) <= 0) {
+        document.getElementById("principal").focus();
+        alert("Enter a positive number");
+        return false;
+    }
+
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    var interest = principal * years * rate/100;
+
+    var thisDay = new Date();
+    var thisYear = thisDay.getFullYear();
+    var endYear = Number(thisYear) + Number(years);
+    
+    //var resultText = "test <br> break" //principal + " " + rate + " " + years + " " + interest + " " + endYear;
+    var resultText = "If you deposit " +  "<mark>" + String(principal) + "</mark>" + "<br>" + "at an interest rate of " + "<mark>" + String(rate) + "</mark>" + "," + "<br>" + "you will receive an amount of " + "<mark>" + String(interest) + "</mark>" + "<br>" + "in the year " + "<mark>" + String(endYear) + "</mark>" + ".";
+    document.getElementById("result").innerHTML = resultText;
+
+    document.getElementById("principal").focus();
+}
+
+function showrate()
+{
+    var rate = principle = document.getElementById("rate").value;
+    document.getElementById("ratespan").innerHTML = String(rate).concat("%");
 }
         
